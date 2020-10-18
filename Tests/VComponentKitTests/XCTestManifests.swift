43d00ce8845dee0755434b81bd1fc53dde0fde75
fwcd <fwcdmail@gmail.com>
2020-10-18 17:09:32 +0200

    Add unit test for string utilities

--- Tests/VComponentKitTests/XCTestManifests.swift
+++ Tests/VComponentKitTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(VComponentTests.allTests),
+        testCase(StringUtilitiesTests.allTests)
     ]
 }
 #endif
